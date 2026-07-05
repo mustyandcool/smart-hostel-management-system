f0b636965f275a10f02ec07604627413bb322bd8
mustyandcool <mustyandcool@gmail.com>
2026-07-05 13:53:28 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,4 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-echo "Smart Hostel System is working!";
+require_once 'includes/config.php';
+require_once 'includes/db.php';
+
+echo "Database Connected Successfully!";
