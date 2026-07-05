c8c85cefb130dfbb0b4e99bd679376f1d928dad3
mustyandcool <mustyandcool@gmail.com>
2026-07-05 20:00:22 +0100

    modified

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,26 @@
 <?php
 
-define('DB_HOST', 'sql301.infinityfree.com');
+// ===============================
+// DATABASE CONFIGURATION
+// ===============================
+
+define('DB_HOST', 'sql312.infinityfree.com');
 define('DB_NAME', 'if0_42340667_smart_hostel_db');
 define('DB_USER', 'if0_42340667');
 define('DB_PASS', 'Musty2026');
+
+// ===============================
+// SITE CONFIGURATION
+// ===============================
+
+define('SITE_NAME', 'Smart Hostel Allocation and Management System');
+define('SITE_URL', 'https://smarthostel.lovestoblog.com');
+
+// ===============================
+// UPLOAD SETTINGS
+// ===============================
+
+define('UPLOAD_PATH', 'assets/uploads/');
+define('MAX_FILE_SIZE', 2 * 1024 * 1024); // 2MB
 
 ?>
